798cccb457c08eba76f9fa891f7ea4ff3e19ccda
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-11-07 09:44:26 +0700

    CGRAPH_ERROR macro and handler function

---
+++ cgraph_error.c
@@ -0,0 +1,18 @@
+#include "cgraph_error.h"
+
+static cgraph_error_handler_t *cgraph_i_error_handler = 0;
+
+int igraph_error(const char *reason, const char *file, int line) {
+
+    if (cgraph_i_error_handler) {
+        cgraph_i_error_handler(reason, file, line);
+    }
+    return 0;
+}
+
+cgraph_error_handler_t * cgraph_set_error_handler(
+          cgraph_error_handler_t * new_handler) {
+    cgraph_error_handler_t * previous_handler = cgraph_i_error_handler;
+    cgraph_i_error_handler = new_handler;
+    return previous_handler;
+}
