300a3ccdf61e4b2f724dced79d7266e209b4c41b
unknown <hieu.xteen28@gmail.com>
2020-11-08 20:31:24 +0700

    better perfomance

--- examples/demo.c
+++ examples/demo.c
@@ -3,31 +3,38 @@
 #include "cgraph.h"
 
 
+void print_vector(cvector_vector_type(int) v) {
+	printf("v(%lu): {", cvector_size(v));
+	for (int i = 0; i < cvector_size(v); ++i) {
+		if (i == 0) {
+			printf("%d", v[i]);
+		} 
+		else {
+			printf(", %d", v[i]);
+		}
+	}
+	printf("}\n");
+}
+
 int main()
 {
     cgraph_t g;
     cgraph_ivec_t v = NULL;
-    cvector_push_back(v, 0);
-    cvector_push_back(v, 1);
-    cvector_push_back(v, 0);
-    cvector_push_back(v, 3);
-    cvector_push_back(v, 1);
-    cvector_push_back(v, 2);
-    cvector_push_back(v, 1);
-    cvector_push_back(v, 3);
-    cvector_push_back(v, 2);
-    cvector_push_back(v, 4);
-    cvector_push_back(v, 0);
-    cvector_push_back(v, 2);
-    cvector_push_back(v, 3);
-    cvector_push_back(v, 5);
-    cvector_push_back(v, 3);
-    cvector_push_back(v, 1);
-    cvector_push_back(v, 1);
-    cvector_push_back(v, 4);
+    int push;
+    FILE * a = fopen("input.txt", "r");
+    while(fscanf(a, "%d", &push) != EOF) {
+        cvector_push_back(v, push);
+    }
+    fclose(a);
 
-    cgraph_create(&g, v, 0, true);
-    // print_vector(g.from);
+    cgraph_create(&g, v, 0, false);
     
+    print_vector(g.from);
+    print_vector(g.to);
+    print_vector(g.oi);
+    print_vector(g.ii);
+    print_vector(g.os);
+    print_vector(g.is);
+    // cgraph_destroy(&g);
     return 0;
 }
