43fd02879ce3d1684a19ded318b573fbec418be0
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-11-19 23:44:44 +0700

    Unit test: cgraph_ivec_order

---
+++ tests/cgraph_vector/test_cgraph_ivec_order.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include "cgraph_vector.h"
+
+cgraph_ivec_t cgraph_ivec_fromarray(CGRAPH_INTEGER *a, CGRAPH_INTEGER n) {
+  cgraph_ivec_t v = cvector_create_empty();
+  for (CGRAPH_INTEGER i = 0; i < n; ++i) {
+    cvector_push_back(v, a[i]);
+  }
+  return v;
+}
+
+bool cgraph_ivec_equal(cgraph_ivec_t v, CGRAPH_INTEGER *a) {
+  CGRAPH_INTEGER n = cvector_size(v);
+  for (int i = 0; i < n; ++i) {
+    if (v[i] != a[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  cgraph_ivec_t v = cgraph_ivec_fromarray((CGRAPH_INTEGER []){1, 0, 2, 2, 3, 1}, 6);
+  cgraph_ivec_t v2 = cgraph_ivec_fromarray((CGRAPH_INTEGER []){3, 2, 1, 3, 2, 0}, 6);
+  cgraph_ivec_t res = cvector_create_empty();
+  cgraph_ivec_init(res, 6);
+  cgraph_ivec_order(v, v2, res);
+  if (cvector_size(res) != 6) {
+    printf("%s: Failed\n", "Test ivec init size");
+    return 1;
+  }
+  if (!cgraph_ivec_equal(res, (CGRAPH_INTEGER[]){1, 5, 0, 2, 3, 4})) {
+    printf("%s: Failed\n", "Test index");
+    return 1;
+  }
+  printf("%s: OK\n", "cgraph_ivec_order test");
+  return 0;
+}
