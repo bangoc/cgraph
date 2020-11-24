79588211030b67fcadcab782556c317dd7a1b15d
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-11-24 23:02:06 +0700

    Test: cgraph_queue

--- cgraph_queue.c
+++ cgraph_queue.c
@@ -7,3 +7,35 @@ cgraph_iqueue_t cgraph_queue_create() {
   }
   return NULL;
 }
+
+int cgraph_iqueue_peek (cgraph_iqueue_const_t const q, CGRAPH_INTEGER *out) {
+  void *data;
+  if (queue_peek(q, &data) == CC_ERR_OUT_OF_RANGE) {
+    return 1;
+  }
+  *out = *((CGRAPH_INTEGER*)data);
+  return 0;
+}
+
+int cgraph_iqueue_poll (cgraph_iqueue_t q, CGRAPH_INTEGER *out) {
+  void *data;
+  if (queue_poll(q, &data) == CC_ERR_OUT_OF_RANGE) {
+    return 1;
+  }
+  *out = *((CGRAPH_INTEGER*)data);
+  return 0;
+}
+
+int cgraph_iqueue_enqueue(cgraph_iqueue_t q, CGRAPH_INTEGER element) {
+  CGRAPH_INTEGER *data = (CGRAPH_INTEGER*)malloc(sizeof(CGRAPH_INTEGER));
+  *data = element;
+  if (queue_enqueue(q, data) == CC_ERR_ALLOC) {
+    free(data);
+    return 1;
+  }
+  return 0;
+}
+
+size_t cgraph_iqueue_size(cgraph_iqueue_const_t const q) {
+  return queue_size(q);
+}
--- cgraph_queue.h
+++ cgraph_queue.h
@@ -13,10 +13,10 @@ typedef const Queue *cgraph_iqueue_const_t;
 
 cgraph_iqueue_t cgraph_queue_create();
 
-int cgraph_iqueue_peek (cgraph_iqueue_const_t const q, CGRAPH_INTEGER *out);
-int cgraph_iqueue_poll (cgraph_iqueue_t q, CGRAPH_INTEGER *out);
+int cgraph_iqueue_peek(cgraph_iqueue_const_t const q, CGRAPH_INTEGER *out);
+int cgraph_iqueue_poll(cgraph_iqueue_t q, CGRAPH_INTEGER *out);
 int cgraph_iqueue_enqueue(cgraph_iqueue_t q, CGRAPH_INTEGER element);
 
-size_t cgraph_iqueue_size(cgraph_iqueue_const_t const queue);
+size_t cgraph_iqueue_size(cgraph_iqueue_const_t const q);
 
 #endif  // CGRAPH_QUEUE_H_
