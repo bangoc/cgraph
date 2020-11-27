82ca4011170ddb32e0be9c7b12095b0232b52ae9
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-11-27 14:54:25 +0700

    cgraph APIs: is_dag

---
+++ cgraph_topology.h
@@ -0,0 +1,9 @@
+#ifndef CGRAPH_TOPOLOGY_H_
+#define CGRAPH_TOPOLOGY_H_
+
+#include "cgraph_datatype.h"
+
+int cgraph_is_dag(const cgraph_t *graph, bool *res);
+
+
+#endif  // CGRAPH_TOPOLOGY_H_
---
+++ structural_properties.c
@@ -0,0 +1,5 @@
+#include "cgraph_topology.h"
+
+int cgraph_is_dag(const cgraph_t *graph, bool *res) {
+  return 0;
+}
