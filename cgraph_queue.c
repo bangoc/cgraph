248344172cc7655b6d72ea49282767aaa0657aa2
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2020-12-02 13:21:11 +0700

    Refactor: cgraph_queue error_code checking & add cgraph_stack test to the list

--- cgraph_queue.c
+++ cgraph_queue.c
@@ -2,7 +2,7 @@
 
 cgraph_iqueue_t cgraph_iqueue_create() {
   cgraph_iqueue_t q;
-  if (queue_new(&q) != CC_ERR_ALLOC) {
+  if (queue_new(&q) == CC_OK) {
     return q;
   }
   return NULL;
@@ -10,30 +10,34 @@ cgraph_iqueue_t cgraph_iqueue_create() {
 
 int cgraph_iqueue_peek (cgraph_iqueue_const_t const q, CGRAPH_INTEGER *out) {
   void *data;
-  if (queue_peek(q, &data) == CC_ERR_OUT_OF_RANGE) {
-    return 1;
+  if (queue_peek(q, &data) == CC_OK) {
+    if (out) {
+      *out = *((CGRAPH_INTEGER*)data);
+    }
+    return 0;
   }
-  *out = *((CGRAPH_INTEGER*)data);
-  return 0;
+  return 1;
 }
 
 int cgraph_iqueue_poll (cgraph_iqueue_t q, CGRAPH_INTEGER *out) {
   void *data;
-  if (queue_poll(q, &data) == CC_ERR_OUT_OF_RANGE) {
-    return 1;
-  }
-  *out = *((CGRAPH_INTEGER*)data);
-  return 0;
+  if (queue_poll(q, &data) == CC_OK) {
+    if (out) {
+      *out = *((CGRAPH_INTEGER*)data);
+    }
+    return 0;
+  }  
+  return 1;
 }
 
 int cgraph_iqueue_enqueue(cgraph_iqueue_t q, CGRAPH_INTEGER element) {
   CGRAPH_INTEGER *data = (CGRAPH_INTEGER*)malloc(sizeof(CGRAPH_INTEGER));
   *data = element;
-  if (queue_enqueue(q, data) == CC_ERR_ALLOC) {
-    free(data);
-    return 1;
+  if (queue_enqueue(q, data) == CC_OK) {
+    return 0;
   }
-  return 0;
+  free(data);
+  return 1;
 }
 
 size_t cgraph_iqueue_size(cgraph_iqueue_const_t const q) {
