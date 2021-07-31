2451af68063ce5cee6896bfe922156074011ff8d
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-07-31 23:30:11 +0700

    Kiểm thử cgraph_add_vertices

---
+++ tests/type_indexededgelist/test_cgraph_add_vertices.c
@@ -0,0 +1,18 @@
+#include "cgraph.h"
+#include "cgen/tests/base/utils.h"
+#include "tests/base/ut.h"
+
+int main() {
+  cgraph_t g = cgraph_create_empty(0, true);
+  CHECK_MSG(cgraph_vcount(g) == 0, "Số đỉnh của đồ thị rỗng bằng 0");
+  cgraph_add_vertices(g, 10);
+  CHECK_MSG(cgraph_vcount(g) == 10, "Sau khi thêm 10 đỉnh");
+  CHECK_MSG(cgraph_add_vertices(g, -10) == CGRAPH_FAILURE, "Lỗi thêm số đỉnh âm");
+
+  CHECK_MSG(cgraph_vcount(g) == 10, "Số đỉnh giữ nguyên sau khi thêm số đỉnh âm");
+
+  cgraph_add_vertices(g, 20);
+  CHECK_MSG(cgraph_vcount(g) == 30, "Sau khi thêm tiếp 20 đỉnh");
+  cgraph_destroy(&g);
+  return 0;
+}
